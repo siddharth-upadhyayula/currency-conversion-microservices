1de3c81b439f89499364e2911fa8627c81ef0ace
Sai Siddharth Upadhyayula <usaisiddarth@gmail.com>
2024-06-21 12:12:38 -0400

    Adding microservice folders

---
+++ limits-microservice/src/main/java/com/sidupadhyayula/microservices/LimitsMicroserviceApplication.java
@@ -0,0 +1,14 @@
+package com.sidupadhyayula.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class LimitsMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LimitsMicroserviceApplication.class, args);
+	}
+
+}
