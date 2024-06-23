4aa8366e1959c82bea9f00f0a7e21aa461ee1737
Sai Siddharth Upadhyayula <usaisiddarth@gmail.com>
2024-06-23 17:52:20 -0400

    instantiate load balancer through naming server and create api gateway

--- currency-conversion-service/src/main/java/com/sidupadhyayula/currency_conversion_service/CurrencyConversionServiceApplication.java
+++ currency-conversion-service/src/main/java/com/sidupadhyayula/currency_conversion_service/CurrencyConversionServiceApplication.java
@@ -3,9 +3,11 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableEurekaServer
 public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
